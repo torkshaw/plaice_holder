52e36be30480c6f97e9761622bacc18d744fbf96
Winter <winterj1506@gmail.com>
2026-03-25 23:34:05 +0000

    Winter 25 03 Main Menu UI (#18)
    * main menu stuff
    
    noodling around with layouts, added debug reset button
    
    * mainmenu
    
    update
    
    * more main menu stuff
    
    added buttons
    added scripting to start game and quit buttons
    added canvases for sub-menus
    
    * build profile
    
    added all level scenes to build profile
    
    * placeholder sub-menus
    
    placeholder text on sub-menus
    functionality added to buttons to ensure only one sub-menu is active at a time
    added colliders to prevent any accidental horizontal movement of menu
    
    * lil tweak
    
    changed load scene to load scene async to avoid stutter on load

---
+++ Assets/-- Assets_UI_System WJ/UI_System_Scripts/MainMenuController_WJ.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private string level1;
+    
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(level1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}// end of class
+// winter james
