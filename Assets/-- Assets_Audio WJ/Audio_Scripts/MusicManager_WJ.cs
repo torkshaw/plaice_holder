14625e0812d0f74cab786f4572f28e57f0dcda50
Winter <winterj1506@gmail.com>
2026-03-28 16:13:38 +0000

    audiomanager scripting

---
+++ Assets/-- Assets_Audio WJ/Audio_Scripts/MusicManager_WJ.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+
+{
+    public bool winter_OST = true;
+    public AudioSource musicPlayer;
+
+    [Header("Winter's Music")]
+    public AudioClip[] musicWinter;
+
+
+    //[Header("Ella's Music")]
+    //public AudioClip[] musicElla;
+
+    private void Awake()
+    {
+        winter_OST = true;
+        DontDestroyOnLoad(gameObject);
+        musicPlayer = GetComponent<AudioSource>();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        musicPlayer.volume = 0;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (winter_OST == true)
+        {
+            PlaySceneMusicWinter();
+        }
+        else
+        {
+        //    PlaySceneMusicElla();
+        }
+        
+
+    }
+
+    void PlaySceneMusicWinter()
+    {
+
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (sceneIndex < musicWinter.Length)
+        {
+            musicPlayer.clip = musicWinter[sceneIndex];
+            musicPlayer.Play();
+        }
+        else
+        {
+            Debug.Log("No music assigned for scene");
+        }
+    }
+
+    //void PlaySceneMusicElla()
+    //{
+
+        //int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //if (sceneIndex < musicElla.Length)
+        //{
+       //     musicPlayer.clip = musicElla[sceneIndex];
+        //    musicPlayer.Play();
+       // }
+     //  else
+       // {
+       //     Debug.Log("No music assigned for scene");
+       // }
+   // }
+}// end of class
+// winter james
