78fe2cd9c09eb4716dc946cc6446cd6a3120a043
Winter <winterj1506@gmail.com>
2026-04-21 16:18:16 +0100

    misc ui tweaking (#38)
    ui tweaks for gdd sscreenshots

--- Assets/-- Assets_UI_System WJ/UI_System_Scripts/MenuDebug_WJ.cs
+++ Assets/-- Assets_UI_System WJ/UI_System_Scripts/MenuDebug_WJ.cs
@@ -2,11 +2,51 @@ using UnityEngine;
 using UnityEngine.SceneManagement; // to allow reloading of scene
 public class MenuDebug : MonoBehaviour
 {
+    [SerializeField] private string levelSelect1;
+    [SerializeField] private string levelSelect2;
+    [SerializeField] private string levelSelect3;
+    [SerializeField] private string levelSelect4;
+    [SerializeField] private string levelSelect5;
+
+    private void Update()
+    {
+        LevelSelect();
+    }
+
     public void ResetMenu() // Reset the current scene
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // capture the current scene and reload it       
 
     } // end ResetMenu
+
+    public void LevelSelect()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            SceneManager.LoadSceneAsync(levelSelect1);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F2))
+        {
+            SceneManager.LoadSceneAsync(levelSelect2);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F3))
+        {
+            SceneManager.LoadSceneAsync(levelSelect3);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F4))
+        {
+            SceneManager.LoadSceneAsync(levelSelect4);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F5))
+        {
+            SceneManager.LoadSceneAsync(levelSelect5);
+        }
+
+    }
 
 }// end of class
 // winter james
