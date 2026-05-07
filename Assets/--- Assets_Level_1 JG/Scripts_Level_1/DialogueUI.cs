493fb54a0106f9dba3e155b8f5af82c3af46439c
Johnvgriffin <jgek@hotmail.com>
2026-05-07 11:36:50 +0100

    Dialogue System Update (#62)
    * DialogueSystem
    
    * Continuation of Dialogue System
    
    * fin
    
    * Update LiberationSans SDF - Fallback.asset

--- Assets/--- Assets_Level_1 JG/Scripts_Level_1/DialogueUI.cs
+++ Assets/--- Assets_Level_1 JG/Scripts_Level_1/DialogueUI.cs
@@ -1,12 +1,45 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class DialogueUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text textLabel;
+    [SerializeField] private DialogueObject testDialogue;
+    [SerializeField] private GameObject dialogueBox;
+
+    private TypewriterEffect typewriterEffect;
 
     private void Start()
     {
-        textLabel.text = "Hello!\nThis is my second line.";
+        typewriterEffect = GetComponent<TypewriterEffect>();
+        CloseDialogueBox();
+        ShowDialogue(testDialogue);
+    }
+
+    public void ShowDialogue(DialogueObject dialogueObject)
+    {
+        dialogueBox.SetActive(true);
+        StartCoroutine(StepThroughDialogue(dialogueObject));
+    }
+
+    private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
+    {
+        yield return new WaitForSeconds(1);
+
+        foreach (string dialogue in dialogueObject.Dialogue)
+        {
+            yield return typewriterEffect.Run(dialogue, textLabel);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+
+        }
+
+           CloseDialogueBox();
+    }
+
+    private void CloseDialogueBox()
+    {
+        dialogueBox.SetActive(false);
+        textLabel.text = string.Empty;
     }
 }
