7442d5cd17bd5228fa990c860014866627eeaf1d
Johnvgriffin <jgek@hotmail.com>
2026-05-07 11:34:02 +0100

    Continuation of Dialogue System

--- Assets/--- Assets_Level_1 JG/Scripts_Level_1/DialogueObject.cs
+++ Assets/--- Assets_Level_1 JG/Scripts_Level_1/DialogueObject.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class DialogueObject : ScriptableObject
 {
     [SerializeField][TextArea] private string[] dialogue;
+    [SerializeField] private Response[] responses;
 
     public string[] Dialogue => dialogue;
+
+    public Response[] Responses => responses;
 }
