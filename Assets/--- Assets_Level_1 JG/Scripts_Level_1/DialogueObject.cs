ee804ec7260773f7b7b61a5f30cde33fce88d65c
Johnvgriffin <jgek@hotmail.com>
2026-05-07 11:06:47 +0100

    DialogueSystem

---
+++ Assets/--- Assets_Level_1 JG/Scripts_Level_1/DialogueObject.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Dialogue/DialogueObject")]
+
+public class DialogueObject : ScriptableObject
+{
+    [SerializeField][TextArea] private string[] dialogue;
+
+    public string[] Dialogue => dialogue;
+}
