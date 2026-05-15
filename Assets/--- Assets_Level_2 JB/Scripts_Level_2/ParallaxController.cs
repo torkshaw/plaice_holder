cf0d0666fa98ecb2f7070ab9f5602f90138be27e
ReleaseTheBeesAgain <jasonb1234656@gmail.com>
2026-05-15 14:25:05 +0100

    JB_Fixed
    Almost deleted all my objects, reverted changes using stash

--- Assets/--- Assets_Level_2 JB/Scripts_Level_2/ParallaxController.cs
+++ Assets/--- Assets_Level_2 JB/Scripts_Level_2/ParallaxController.cs
@@ -1,27 +1,19 @@
 using UnityEngine;
-
-
 // Script created by Jason 
 public class ParallaxController : MonoBehaviour
 {
-    private float startPos; // Objects starting position in the world (x)
-    private float startCamPos; // Main cams starting x position
-    public GameObject cam;  // Field for Main Cam
-    [SerializeField] private float parallaxEffect; // 0 = static, 1 = follows cam, in between for varied movement speeds
-
-
+    private float startPos;
+    public GameObject cam;
+    [SerializeField] private float parallaxEffect;
 
     void Start()
     {
         startPos = transform.position.x;
-        startCamPos = cam.transform.position.x; // Parallax movement will be dependant on the starting position
-                                                // This fixed objects jumpin backwards on spawning
     }
 
     void Update()
     {
-        float camTravel = cam.transform.position.x - startCamPos;
-        transform.position = new Vector3(startPos + (camTravel * parallaxEffect),
+        transform.position = new Vector3(startPos + (cam.transform.position.x * parallaxEffect),
                                          transform.position.y,
                                          transform.position.z);
     }
