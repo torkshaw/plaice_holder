4f34e2b169fa85511098902dd1f17dd07c6803ab
Winter <winterj1506@gmail.com>
2026-05-18 19:51:23 +0100

    some music and sfx implementation
    damage sfx and jump
    ice and cave level
    title and level one sharing theme for now

--- Assets/-- Assets_Audio WJ/Audio_Scripts/SFXManager_WJ.cs
+++ Assets/-- Assets_Audio WJ/Audio_Scripts/SFXManager_WJ.cs
@@ -1,40 +1,50 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class SFXManager : MonoBehaviour
+public class PlayerSFXManager : MonoBehaviour
 {
-    public static SFXManager instance;
-    public AudioSource menuSFXPlayer;    
-    public AudioClip menuButtonClick;
+    [SerializeField] private PlayerLifeController lifeController;
+    //[SerializeField] private PlayerMovementControllerV2 movementController;
+    //[SerializeField] private FishingRodController2D fishingController;
+    public AudioSource playerSFXPlayer;
 
-    private void Awake()
+    [Header("SFX List")]
+    [SerializeField] private AudioClip enemyDamageSFX;
+    [SerializeField] private AudioClip waterDamageSFX;
+    //[SerializeField] private AudioClip jumpSFX;
+    //[SerializeField] private AudioClip hookedSFX;
+    private void OnEnable()
     {
-        if (instance != null && instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        else
-        {
-            instance = this;
-        }
-        DontDestroyOnLoad(gameObject);
-
-        menuSFXPlayer = GetComponent<AudioSource>(); 
-        SceneManager.sceneLoaded += OnSceneLoaded;
+        lifeController.DamageTaken += EnemyDamageSFX;
+        lifeController.RespawnRequested += WaterDamageSFX;
+        //movementController.OnJump += JumpSFX;
+        //fishingController.AttachToObject += HookedSFX;
+        //fishingController.ReelingStarted += ReelSFX;
     }
 
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    //private void JumpSFX()
+    //{
+    //    playerSFXPlayer.PlayOneShot(jumpSFX);
+    //}
+
+    //private void HookedSFX()
+    //{
+    //    playerSFXPlayer.PlayOneShot(hookedSFX);
+    //}
+
+    //private void ReelSFX()
+    //{
+
+    //}
+
+private void EnemyDamageSFX(Collider2D sourceCollider, Vector2 hitDirection)
     {
-        
+        playerSFXPlayer.PlayOneShot(enemyDamageSFX);
     }
 
-    public void MenuButtonSFX()
+    private void WaterDamageSFX()
     {
-        menuSFXPlayer.clip = menuButtonClick;
-        menuSFXPlayer.Play();
+        playerSFXPlayer.PlayOneShot(waterDamageSFX);
     }
 
-
-}
+}//end of class
+//winter james
